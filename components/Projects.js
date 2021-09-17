69bdf3a8aae392c85a5cbf4c0dbbdbe8c24b2333
T0w3r <torres.alejo2001@gmail.com>
2021-09-17 20:12:22 -0300

    added proyects+imgs

--- components/Projects.js
+++ components/Projects.js
@@ -1,17 +1,17 @@
 import React from "react";
 
 const Projects = ({ project }) => {
-  const { name, description, image } = project;
+  const { name, description, image ,knowMore,bgColor} = project;
   return (
     <div className="col-md-4 p-2">
       <div className="card h-100">
-        <div className="overflow">
+        <div className={"overflow"} style={{backgroundColor:bgColor}}>
           <img src={image} alt="imagen" className="card-img-top" />
         </div>
         <div className="card-body">
           <h3>{name}</h3>
           <p>{description}</p>
-          <a href="#!">Know More</a>
+          <a href={knowMore}>Know More</a>
         </div>
       </div>
     </div>
