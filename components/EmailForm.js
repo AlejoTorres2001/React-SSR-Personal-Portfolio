f2a04b57d8fa16930a467422e50665fdd4ad8ca7
T0w3r <torres.alejo2001@gmail.com>
2021-09-21 17:51:46 -0300

    added animations to emailForm

--- components/EmailForm.js
+++ components/EmailForm.js
@@ -1,6 +1,40 @@
 import React from 'react'
 import emailjs from 'emailjs-com'
+import {motion} from 'framer-motion'
 const EmailForm = () => {
+
+    const cardsVariants={
+        hidden:{
+          y:'-100vw'
+        },
+        visible:{
+          y:0,
+          transition:{
+            type:"spring",
+            duratin:1,
+            bounce:0.3,
+            delay: 0.5,
+            when:"beforeChildren",
+            staggerChildren:0.1
+          }
+        }
+      
+      }
+      const cardElementsVariant={
+        hidden:{
+          x:-10,
+          opacity:0
+        },
+        visible:{
+          x:"0",
+          opacity:1,
+          
+        }
+      }
+
+
+
+
     const sendEmail = (e) => {
         e.preventDefault()
         emailjs.sendForm(process.env.NEXT_PUBLIC_YOUR_SERVICE_ID, process.env.NEXT_PUBLIC_YOUR_TEMPLATE_ID, e.target,process.env.NEXT_PUBLIC_YOUR_USER_ID)
@@ -12,19 +46,19 @@ const EmailForm = () => {
           e.target.reset()
       };
     return (
-        <form className="container-form my-5" onSubmit={sendEmail}>
+        <motion.form variants={cardsVariants} initial="hidden" animate="visible" className="container-form my-5" onSubmit={sendEmail}>
 		<div className="contact-box">
 			<div className="left-form"></div>
 			<div className="right-form">
-				<h2 className="header-form">Contact Me</h2>
-				<input type="text" className="field" name="name" placeholder="Name"/>
-				<input type="text" className="field"  name="email" placeholder="Email"/>
-				<input type="text" className="field"  name="subject" placeholder="Subject"/>
-				<textarea placeholder="Message"  name="message" className="field text-area-form"></textarea>
-				<input type="submit" value="Send" className="btn-form" />
+				<motion.h2 variants={cardElementsVariant} className="header-form">Contact Me</motion.h2>
+				<motion.input variants={cardElementsVariant} type="text" className="field" name="name" placeholder="Name"/>
+				<motion.input variants={cardElementsVariant} type="text" className="field"  name="email" placeholder="Email"/>
+				<motion.input variants={cardElementsVariant} type="text" className="field"  name="subject" placeholder="Subject"/>
+				<motion.textarea variants={cardElementsVariant} placeholder="Message"  name="message" className="field text-area-form"></motion.textarea>
+				<motion.input variants={cardElementsVariant} type="submit" value="Send" className="btn-form" />
 			</div>
 		</div>
-	    </form>
+	    </motion.form>
     )
 }
 
