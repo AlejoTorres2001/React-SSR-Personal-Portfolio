327b77f0750c1734cd32f414c8aae942eab46cc4
T0w3r <torres.alejo2001@gmail.com>
2021-09-21 17:47:53 -0300

    added animations to PostCards

--- components/PostCards.js
+++ components/PostCards.js
@@ -1,10 +1,46 @@
 import React from "react";
 import Link from "next/link";
+import {useInView} from 'react-intersection-observer'
+import {useAnimation} from "framer-motion"
+import {useEffect} from "react";
+import { motion } from "framer-motion";
+
 const PostCards = ({ post }) => {
+  const {ref,inView} = useInView();
+  const animation = useAnimation();
+  const animationChildren = useAnimation();
+  useEffect(() =>
+  {
+    console.log("Us effect in view=",inView)},[inView])
+    if(inView){
+      animation.start({
+        x:0,
+        transition:{
+          type:"spring",duration:1,bounce:0.4
+        }
+      })
+      animationChildren.start({
+        x:0,
+        opacity:1,
+        transition:{
+          type:"spring",
+          duration:1,
+          bounce:0.3,
+          delay: 0.5,
+      }
+    })
+
+
+    }
+    if(!inView){
+      animation.start({x:'-100vw'})
+      animationChildren.start({ x:'-100vw',
+        opacity:0})
+    }
   return (
-    <div className="col-md-4">
-      <div className="card component-bg">
-        <div className="overflow">
+    <div ref={ref} className="col-md-4">
+      <motion.div  animate={animation}  className="card component-bg">
+        <div   className="overflow">
           <img src={post.image} alt="imagen" className="card-img-top" />
         </div>
         <div className="card-body">
@@ -14,7 +50,7 @@ const PostCards = ({ post }) => {
             <button className="btn btn-dark">Read More</button>
           </Link>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 };
