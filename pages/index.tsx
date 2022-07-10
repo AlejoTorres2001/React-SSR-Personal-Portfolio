9b6f7d30327c8964a9e61122165f4f6a7739dc8d
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-07-10 13:37:48 -0300

    added react-fa

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,33 @@
 import type { NextPage } from 'next'
-
-const Home: NextPage = () => {
-  return <div></div>
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebookSquare,faGithub,faInstagram,faLinkedin,faTwitterSquare } from '@fortawesome/free-brands-svg-icons'
+import Link from 'next/link'
+const Profile: NextPage = () => {
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <Link href="#">
+              <FontAwesomeIcon icon={faFacebookSquare} />
+            </Link>
+            <Link href="#">
+              <FontAwesomeIcon icon={faGithub} />
+            </Link>
+            <Link href="#">
+              <FontAwesomeIcon icon={faInstagram} />
+            </Link>
+            <Link href="#">
+              <FontAwesomeIcon icon={faLinkedin} />
+            </Link>
+            <Link href="#">
+              <FontAwesomeIcon icon={faTwitterSquare} />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default Home
+export default Profile
