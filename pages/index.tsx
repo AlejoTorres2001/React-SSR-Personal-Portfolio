123c1e240f6b2dafbac7fc71a0d9e34c1ca2d742
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-07-10 17:22:17 -0300

    styling profile

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,61 +2,72 @@
 import type { NextPage } from 'next'
 import Typical from 'react-typical'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebookSquare,faGithub,faInstagram,faLinkedin,faTwitterSquare } from '@fortawesome/free-brands-svg-icons'
+import {
+  faFacebookSquare,
+  faGithub,
+  faInstagram,
+  faLinkedin,
+  faTwitterSquare
+} from '@fortawesome/free-brands-svg-icons'
 import Link from 'next/link'
 import HeadTag from '../components/HeadTag'
 const Profile: NextPage = () => {
   return (
     <div className="profile-container">
-      <HeadTag/>
+      <HeadTag />
       <div className="profile-parent">
         <div className="profile-details">
-          <div className="colz">
-            <Link href="#">
-              <FontAwesomeIcon icon={faFacebookSquare} />
-            </Link>
-            <Link href="#">
-              <FontAwesomeIcon icon={faGithub} />
-            </Link>
-            <Link href="#">
-              <FontAwesomeIcon icon={faInstagram} />
-            </Link>
-            <Link href="#">
-              <FontAwesomeIcon icon={faLinkedin} />
-            </Link>
-            <Link href="#">
-              <FontAwesomeIcon icon={faTwitterSquare} />
-            </Link>
+          <div className="cols">
+            <div className="cols-icons">
+              <Link  href="#">
+                <FontAwesomeIcon className='icon' icon={faFacebookSquare} />
+              </Link>
+              <Link href="#">
+                <FontAwesomeIcon className='icon' icon={faGithub} />
+              </Link>
+              <Link href="#">
+                <FontAwesomeIcon className='icon' icon={faInstagram} />
+              </Link>
+              <Link href="#">
+                <FontAwesomeIcon className='icon' icon={faLinkedin} />
+              </Link>
+              <Link href="#">
+                <FontAwesomeIcon className='icon' icon={faTwitterSquare} />
+              </Link>
+            </div>
           </div>
           <div className="profile-details-name">
-            <span className='primary-text'>
-              {""}
-              Hello, I'm <span className='highlighted-text'>Alejo</span>
+            <span className="primary-text">
+              {''}
+              Hello, I'm <span className="highlighted-text">Alejo</span>
             </span>
           </div>
           <div className="profile-details-role">
-            <span className='primary-text'>
-              {""}
+            <span className="primary-text">
+              {''}
               <h1>
-              {""}
-                <Typical
-                loop={Infinity}
-                steps={[
-                  'hola',
-                  1000,
-                  'chau',
-                  1000,
-                ]}
-                />
+                {''}
+                <Typical loop={Infinity} steps={['hola', 1000, 'chau', 1000]} />
               </h1>
-              <span className='profile-role-tagline'>
-              knack of building applications with front and back-end operations.
+              <span className="profile-role-tagline">
+                knack of building applications with front and back-end
+                operations.
               </span>
             </span>
           </div>
           <div className="profile-options">
-            <button className='btn btn-primary'></button>
-            <button className='btn btn-primary'></button>
+            <button className="btn primary-btn">
+              {''}
+              Hire Me
+            </button>
+            <a href="#">
+              <button className="btn highlighted-btn">Get Resume</button>
+            </a>
+          </div>
+        </div>
+        <div className="profile-picture">
+          <div className="profile-picture-background">
+
           </div>
         </div>
       </div>
