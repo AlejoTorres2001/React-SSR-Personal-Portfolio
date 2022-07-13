bc8aaaf777d3788595d99e5844dd6237237d1de6
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-07-13 14:35:54 -0300

    adding badge component

--- components/Badge.tsx
+++ components/Badge.tsx
@@ -2,7 +2,50 @@ import React from 'react'
 
 const Badge = () => {
   return (
-    <div>Badge</div>
+    <blockquote className="badgr-badge">
+      <a href="https://api.badgr.io/public/assertions/ardud1z8QI-s79TNBby1YA?identity__email=torres.alejo2001%40gmail.com">
+        <img
+          width="120px"
+          height="120px"
+          src="https://api.badgr.io/public/assertions/ardud1z8QI-s79TNBby1YA/image"
+        />
+      </a>
+      <p
+        className="badgr-badge-name"
+      >
+        Event Streaming Workshop
+      </p>
+      <p
+        className="badgr-badge-date"
+      >
+        <strong className='strong-text'>
+          Awarded:{' '}
+        </strong>
+        Nov 22, 2021
+      </p>
+      <p
+        className="badgr-badge-recipient"
+      >
+        <strong className='strong-text'>
+          Awarded To:{' '}
+        </strong>
+        <span style={{ display: 'block' }}> Alejo Torres</span>
+      </p>
+      <p style={{ margin: '16px 0', padding: '0' }}>
+        <a
+          className="badgr-badge-verify"
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://badgecheck.io?url=https%3A%2F%2Fapi.badgr.io%2Fpublic%2Fassertions%2Fardud1z8QI-s79TNBby1YA%3Fidentity__email%3Dtorres.alejo2001%2540gmail.com&amp;identity__email=torres.alejo2001%40gmail.com"
+        >
+          VERIFY
+        </a>
+      </p>
+      <script
+        async
+        src="https://badgr.com/assets/widgets.bundle.js"
+      ></script>
+    </blockquote>
   )
 }
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import '../styles/Profile.css'
 import '../styles/AboutMe.css'
 import '../styles/Resume.css'
 import '../styles/Badges.css'
+import '../styles/Badge.css'
 import type { AppProps } from 'next/app'
 import '@fortawesome/fontawesome-svg-core/styles.css' // import Font Awesome CSS
 import { config } from '@fortawesome/fontawesome-svg-core'
