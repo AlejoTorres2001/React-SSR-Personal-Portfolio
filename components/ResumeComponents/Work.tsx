ca1d4734fc96536221f3683e9a4ed641347045a7
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-07-13 17:42:03 -0300

    adding work experience

--- components/ResumeComponents/Work.tsx
+++ components/ResumeComponents/Work.tsx
@@ -7,131 +7,126 @@ const Work: React.FunctionComponent<object> = () => {
         <div className="resume-heading">
           <div className="resume-main-heading">
             <div className="heading-bullet"></div>
-            <span>WRITE SOMETHING</span>
-            <div className="heading-date">2021-Present</div>
+            <span>Machine Learning Researcher</span>
+            <div className="heading-date">2022-Present</div>
           </div>
           <div className="resume-sub-heading">
-            <span>ASDASDASD</span>
+            <span>Instituto de Innovacion y Tecnologia Aplicada (IITA)</span>
           </div>
           <div className="resume-heading-description">
-            <span></span>
+            <span>
+              Developed a Deep-Q learning agent to be used for maze exploration
+              in Robocup Competition
+            </span>
           </div>
         </div>
         <div className="experience-description">
           <span className="resume-description-text">
-            CURRENTLY DOING ASDASDASD.
+            - Research and document AI models <br />
+            - Develop a simplified testing environment for AI models <br />
+            - Develop a Deep-Q Learning Model
+            <br />
+            - Record the learning process and tune hyperparameters using Google
+            Cloud Compute Engine + MLFlow
+            <br />
           </span>
         </div>
+        <div className="resume-heading">
+          <div className="resume-main-heading">
+            <div className="heading-bullet"></div>
+            <span>Developer and Technical support</span>
+            <div className="heading-date">2021-2021</div>
+          </div>
+          <div className="resume-sub-heading">
+            <span>Digio SRL</span>
+          </div>
+          <div className="resume-heading-description">
+            <span>
+              Developed Web-based systems using ASP.NET fremawork and Genexus11
+            </span>
+          </div>
+        </div>
         <div className="experience-description">
-          <span className="resume-description-text">- SOMETHING 1.</span>
+          <span className="resume-description-text"></span>
+        </div>
+        <div className="experience-description">
+          <span className="resume-description-text">
+            - Developing and mantaining existing systems using ASP.NET and
+            Genexus11
+          </span>
           <br />
-          <span className="resume-description-text">- SOMETHING 2 </span>
+          <span className="resume-description-text">
+            - Technical support over production systems{' '}
+          </span>
           <br />
-          <span className="resume-description-text">- SOMETHING 3</span>
+          <span className="resume-description-text">
+            - Documentation of solutions and customer service processes
+          </span>
           <br />
         </div>
         <div className="resume-heading">
           <div className="resume-main-heading">
             <div className="heading-bullet"></div>
-            <span>WRITE SOMETHING</span>
+            <span>Student Researcher</span>
             <div className="heading-date">2021-Present</div>
           </div>
           <div className="resume-sub-heading">
-            <span>ASDASDASD</span>
+            <span>Engineering Faculty at UCASAL</span>
           </div>
           <div className="resume-heading-description">
-            <span></span>
+            <span>Digital Forensics and Research</span>
           </div>
+        </div>
+        <div className="experience-description">
+          <span className="resume-description-text"></span>
         </div>
         <div className="experience-description">
           <span className="resume-description-text">
-            CURRENTLY DOING ASDASDASD.
+            - Participation in the development of the KeyWordFinder software
+            using the frameworks electron.js + Vue.js
           </span>
-        </div>
-        <div className="experience-description">
-          <span className="resume-description-text">- SOMETHING 1.</span>
           <br />
-          <span className="resume-description-text">- SOMETHING 2 </span>
+          <span className="resume-description-text">
+            - Record progress and prepare weekly reports
+          </span>
           <br />
-          <span className="resume-description-text">- SOMETHING 3</span>
+          <span className="resume-description-text">
+            - Investigate possible implementation improvements following the
+            work plan
+          </span>
           <br />
         </div>
         <div className="resume-heading">
           <div className="resume-main-heading">
             <div className="heading-bullet"></div>
-            <span>WRITE SOMETHING</span>
+            <span>Python Programming Tutor</span>
             <div className="heading-date">2021-Present</div>
           </div>
           <div className="resume-sub-heading">
-            <span>ASDASDASD</span>
+            <span>Instituto de Innovacion y Tecnologia Aplicada (IITA)</span>
           </div>
           <div className="resume-heading-description">
-            <span></span>
+            <span>
+              Prepared Lectures and Dictated several courses instances
+            </span>
           </div>
+        </div>
+        <div className="experience-description">
+          <span className="resume-description-text"></span>
         </div>
         <div className="experience-description">
           <span className="resume-description-text">
-            CURRENTLY DOING ASDASDASD.
+            - Prepare, teach and correct the weekly course activities
           </span>
-        </div>
-        <div className="experience-description">
-          <span className="resume-description-text">- SOMETHING 1.</span>
           <br />
-          <span className="resume-description-text">- SOMETHING 2 </span>
-          <br />
-          <span className="resume-description-text">- SOMETHING 3</span>
-          <br />
-        </div>
-        <div className="resume-heading">
-          <div className="resume-main-heading">
-            <div className="heading-bullet"></div>
-            <span>WRITE SOMETHING</span>
-            <div className="heading-date">2021-Present</div>
-          </div>
-          <div className="resume-sub-heading">
-            <span>ASDASDASD</span>
-          </div>
-          <div className="resume-heading-description">
-            <span></span>
-          </div>
-        </div>
-        <div className="experience-description">
           <span className="resume-description-text">
-            CURRENTLY DOING ASDASDASD.
+            - Assist and develop empathy with students to encourage a active
+            participation
           </span>
-        </div>
-        <div className="experience-description">
-          <span className="resume-description-text">- SOMETHING 1.</span>
           <br />
-          <span className="resume-description-text">- SOMETHING 2 </span>
-          <br />
-          <span className="resume-description-text">- SOMETHING 3</span>
-          <br />
-        </div>
-        <div className="resume-heading">
-          <div className="resume-main-heading">
-            <div className="heading-bullet"></div>
-            <span>WRITE SOMETHING</span>
-            <div className="heading-date">2021-Present</div>
-          </div>
-          <div className="resume-sub-heading">
-            <span>ASDASDASD</span>
-          </div>
-          <div className="resume-heading-description">
-            <span></span>
-          </div>
-        </div>
-        <div className="experience-description">
           <span className="resume-description-text">
-            CURRENTLY DOING ASDASDASD.
+            - Keep track of student progress for presentation to superiors
           </span>
-        </div>
-        <div className="experience-description">
-          <span className="resume-description-text">- SOMETHING 1.</span>
-          <br />
-          <span className="resume-description-text">- SOMETHING 2 </span>
-          <br />
-          <span className="resume-description-text">- SOMETHING 3</span>
           <br />
         </div>
       </div>
