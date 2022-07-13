0f03740961c727bf132689c58c47642f82f1e749
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-07-12 21:22:04 -0300

    added badges carousel

--- components/Badges.tsx
+++ components/Badges.tsx
@@ -1,26 +1,39 @@
 import React from 'react'
-import Footer from '../components/Footer'
+import Carousel from 'react-elastic-carousel'
 const Badges = () => {
+  const breakPoints = [
+    { width: 1, itemsToShow: 1 },
+    { width: 550, itemsToShow: 2 },
+    { width: 768, itemsToShow: 3 },
+    { width: 1200, itemsToShow: 4 }
+  ];
   return (
-    <div className='badges-section'>
-    <div className="heading-container-badges">
-      <div className="screen-heading">
-        <span>Badges</span>
-      </div>
-      <div className="screen-sub-heading">
-        <span>Workshops Hackathons and Challenges</span>
-      </div>
-      <div className="heading-seperator">
-        <div className="seperator-line"></div>
-        <div className="seperator-blob">
-          <div></div>
+    <div className="badges-section">
+      <div className="heading-container-badges">
+        <div className="screen-heading">
+          <span>Badges</span>
+        </div>
+        <div className="screen-sub-heading">
+          <span>Workshops Hackathons and Challenges</span>
+        </div>
+        <div className="heading-seperator">
+          <div className="seperator-line"></div>
+          <div className="seperator-blob">
+            <div></div>
+          </div>
         </div>
       </div>
-    </div>
 
-    <div className="badges-container">
-      <h1>aHola</h1>
-    </div>
+      <div className="badges-container">
+        <Carousel breakPoints={breakPoints}>
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+          <div>5</div>
+          <div>6</div>
+        </Carousel>
+      </div>
     </div>
   )
 }
