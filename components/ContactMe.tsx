0cc7c63403d62cb7efc5189e8e4781f0b8a477b4
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-07-14 14:07:35 -0300

    contactMe frontend

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -1,8 +1,9 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import Typical from 'react-typical'
 import Icons from './Icons'
-
-const ContactMe = () => {
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons'
+const ContactMe: React.FunctionComponent<object> = () => {
   return (
     <div className="main-container">
       <div className="heading-container">
@@ -25,6 +26,26 @@ const ContactMe = () => {
             <Typical steps={['Get in Touch 📧']} />
           </h2>{' '}
           <Icons />
+        </div>
+        <div className="back-form">
+          <div className="img-back">
+            <h4>Send Your Email Here!</h4>
+            <img src="/assets/ContactMe/mail.jpeg" alt="image not found" />
+          </div>
+          <form>
+            <p></p>
+            <label htmlFor="name">Name</label>
+            <input typeof="text" value="" />
+            <label htmlFor="email">Email</label>
+            <input typeof="email" value="" />
+            <label htmlFor="message">Message</label>
+            <textarea typeof="text"></textarea>
+            <div className="send-btn">
+              <button typeof="submit">
+                send <FontAwesomeIcon icon={faPaperPlane}></FontAwesomeIcon>
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
