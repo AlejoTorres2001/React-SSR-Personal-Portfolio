d4b2f8dae4bd2d328b6ca20bec3ebd287c92c86f
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-07-25 12:10:33 -0300

    added language feature frontend

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
 import type { NextPage } from 'next'
-
+import Flag from 'react-world-flags'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import Profile from '../components/Profile'
@@ -12,22 +12,58 @@ import { useState } from 'react'
 const Home: NextPage = () => {
   const [showMobileMenu, setShowMobileMenu] = useState(false)
   const [isLanguagesSelected, setIsLanguagesSelected] = useState(false)
+  const [selectedLanguage, setSelectedLanguage] = useState({
+    language: 'en',
+    flag: '840',
+    height: '14'
+  })
   return (
     <div className="home-container">
       {!showMobileMenu && (
         <div className="language-container">
           <span
             style={{ cursor: 'pointer' }}
-            onClick={() => setIsLanguagesSelected(!isLanguagesSelected)}
+            onClick={() => {
+              setIsLanguagesSelected(!isLanguagesSelected)
+            }}
           >
-            Language
+            <Flag
+              code={selectedLanguage.flag}
+              height={selectedLanguage.height}
+            />
           </span>
         </div>
       )}
       {isLanguagesSelected && !showMobileMenu && (
         <div className="language-options">
-          <span>English</span>
-          <span>Spanish</span>
+          <span
+            className="language-option"
+            onClick={() => {
+              setSelectedLanguage({
+                language: 'sp',
+                flag: '724',
+                height: '18'
+              })
+              setIsLanguagesSelected(!isLanguagesSelected)
+            }}
+          >
+            <Flag code="724" height="18" />
+            &nbsp; Spanish
+          </span>
+          <span
+            className="language-option"
+            onClick={() => {
+              setSelectedLanguage({
+                language: 'en',
+                flag: '840',
+                height: '15'
+              })
+              setIsLanguagesSelected(!isLanguagesSelected)
+            }}
+          >
+            <Flag code="840" height="15" />
+            &nbsp; English
+          </span>
         </div>
       )}
       <Header
