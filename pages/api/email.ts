be0ed7137dab88a32658331b826443a93aefe5c5
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-08-09 12:22:07 -0300

    rejected methods other than POST this route only accepts POST method

--- pages/api/email.ts
+++ pages/api/email.ts
@@ -5,6 +5,11 @@ export default async function handler (
   req: NextApiRequest,
   res: NextApiResponse<object>
 ) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({
+      error: 'Method Not Allowed'
+    })
+  }
   if (!req.body.email || !req.body.message || !req.body.name) {
     return res.status(400).json({ error: 'Missing required fields' })
   }
