9bab37cbc0b22158bd76a038522a4a2f880f0802
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-08-10 11:22:32 -0300

    added footer to /proyects

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from 'next'
 import React, { Dispatch, SetStateAction } from 'react'
+import Footer from '../components/Footer'
 import Header from '../components/Header'
 import Project from '../components/Project'
 import useMobileMenu from '../hooks/useMobileMenu'
@@ -14,22 +15,23 @@ const projects: NextPage = () => {
           setShowMobileMenu as Dispatch<SetStateAction<boolean>>
         }
       />
-<div className="grid ">
-  <Project indexCss={1}/>
-  <Project indexCss={2}/>
-  <Project indexCss={3}/>
-  <Project indexCss={4}/>
-  <Project indexCss={5}/>
-  <Project indexCss={6}/>
-  <Project indexCss={7}/>
-  <Project indexCss={8}/>
-  <Project indexCss={9}/>
-  <Project indexCss={10}/>
-  <Project indexCss={11}/>
-  <Project indexCss={12}/>
-  <Project indexCss={13}/>
-  <Project indexCss={14}/>
-</div>
+      <div className="grid">
+        <Project indexCss={1} />
+        <Project indexCss={2} />
+        <Project indexCss={3} />
+        <Project indexCss={4} />
+        <Project indexCss={5} />
+        <Project indexCss={6} />
+        <Project indexCss={7} />
+        <Project indexCss={8} />
+        <Project indexCss={9} />
+        <Project indexCss={10} />
+        <Project indexCss={11} />
+        <Project indexCss={12} />
+        <Project indexCss={13} />
+        <Project indexCss={14} />
+      </div>
+      <Footer />
     </div>
   )
 }
