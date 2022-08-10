57aaa1462002459be3f6938e0f68c6f7f5660380
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-08-10 11:57:19 -0300

    changing layout

--- components/Project.tsx
+++ components/Project.tsx
@@ -1,14 +1,35 @@
 import React from 'react'
 type props = {
   indexCss: number
+  name: string
+  description: string
+  image: string
+  knowMore?: string
+  bgColor?: string
 }
-const Project = ({ indexCss }:props) => {
+const Project = ({
+  indexCss,
+  name,
+  description,
+  image,
+  knowMore,
+  bgColor
+}: props) => {
+  console.log(image)
   return (
     <div className="item" tabIndex={indexCss}>
-    <div className="box">
-      <div className="diamond"><span>{indexCss}</span></div>
+      <div className="box">
+        <div className="img-container">
+        <img className='image' src={image}/>
+        </div>
+        <div className="title-container">
+          <h3 className="title">{name}</h3>
+        </div>
+        <div className="description-container">
+          <p className="description">{description}</p>
+          </div>
+      </div>
     </div>
-  </div>
   )
 }
 
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -4,7 +4,7 @@ import Footer from '../components/Footer'
 import Header from '../components/Header'
 import Project from '../components/Project'
 import useMobileMenu from '../hooks/useMobileMenu'
-
+import { projectsData } from '../projects.json'
 const projects: NextPage = () => {
   const [showMobileMenu, setShowMobileMenu] = useMobileMenu()
   return (
@@ -16,20 +16,8 @@ const projects: NextPage = () => {
         }
       />
       <div className="grid">
-        <Project indexCss={1} />
-        <Project indexCss={2} />
-        <Project indexCss={3} />
-        <Project indexCss={4} />
-        <Project indexCss={5} />
-        <Project indexCss={6} />
-        <Project indexCss={7} />
-        <Project indexCss={8} />
-        <Project indexCss={9} />
-        <Project indexCss={10} />
-        <Project indexCss={11} />
-        <Project indexCss={12} />
-        <Project indexCss={13} />
-        <Project indexCss={14} />
+        {projectsData.map((project, index) => (<Project indexCss={index + 1} key={index} {...project} />)
+        )}
       </div>
       <Footer />
     </div>
