76aadc43527c585d4fd5364aff1e671fa2360a8e
AlejoTorres2001 <torres.alejo2001@gmail.com>
2025-05-19 18:54:30 -0300

    refactor: restructure MyApp component to include font initialization and improve layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,15 +10,23 @@ import '../styles/ContactMe.css'
 import '../styles/Projects.css'
 import type { AppProps } from 'next/app'
 import '@fortawesome/fontawesome-svg-core/styles.css' // import Font Awesome CSS
-import { config } from '@fortawesome/fontawesome-svg-core' // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
+import { config } from '@fortawesome/fontawesome-svg-core'
 import LanguageContext from '../context/LanguageContextProvider'
+import { Inter } from 'next/font/google'
+
+// Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 config.autoAddCss = false
 
-function MyApp ({ Component, pageProps }: AppProps) {
+// Initialize fonts
+const inter = Inter({ subsets: ['latin'] })
+
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <LanguageContext>
-      <Component {...pageProps} />
-    </LanguageContext>
+    <main className={inter.className}>
+      <LanguageContext>
+        <Component {...pageProps} />
+      </LanguageContext>
+    </main>
   )
 }
 
