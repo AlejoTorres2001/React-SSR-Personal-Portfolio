a1c1694e0e7fd7c7526a6fe4553009ba714dc36e
AlejoTorres2001 <torres.alejo2001@gmail.com>
2025-05-19 19:40:16 -0300

    refactor: replace img tag with next/image component for optimized loading in Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,23 @@
+import Image from 'next/image'
 import React from 'react'
-const Footer:React.FunctionComponent<object> = () => {
+
+const Footer: React.FunctionComponent<object> = () => {
   return (
     <div className='footer-container'>
       <div className='footer-parent'>
-        <img className='footer-image' src='/assets/home/shape.png' alt="division image" />
+        <Image
+          className='footer-image'
+          src='/assets/home/shape.png'
+          width={1920}
+          height={130}
+          alt="division image"
+          priority
+          style={{
+            height: '130px',
+            width: 'auto',
+            maxWidth: '100%'
+          }}
+        />
       </div>
     </div>
   )
