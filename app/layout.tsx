2b7f00b2e9f4491969e0a39554ee84f8f64a9968
AlejoTorres2001 <torres.alejo2001@gmail.com>
2025-05-20 20:55:03 -0300

    fix: refactor script loading in layout.tsx to use next/script for better performance

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,7 @@ import '../styles/Badges.css'
 import '../styles/Badge.css'
 import '../styles/ContactMe.css'
 import '../styles/Projects.css'
+import Script from 'next/script'
 
 // Tell Font Awesome to skip adding the CSS automatically
 config.autoAddCss = false
@@ -22,20 +23,22 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: "Alejo's Portfolio",
-  description: 'Alejo Torres Fullstack Developer',
+  description: 'Alejo Torres Fullstack Developer'
 }
 
-
 export default function RootLayout({
-  children,
+  children
 }: {
   children: React.ReactNode
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-          {children}
-        <script src="https://badgr.com/assets/widgets.bundle.js" async />
+        {children}
+        <Script
+          src="https://badgr.com/assets/widgets.bundle.js"
+          strategy="afterInteractive"
+        />
       </body>
     </html>
   )
