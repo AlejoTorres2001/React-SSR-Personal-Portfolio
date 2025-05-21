45d2dcd1d7615f30dec4801f5a74c15ab273285b
AlejoTorres2001 <torres.alejo2001@gmail.com>
2025-05-20 21:50:06 -0300

    Refactor styles: Consolidate CSS files and restructure layout
    - Removed individual style imports in layout.tsx and replaced with a single main.css import.
    - Created new base styles (_gobal.css, _reset.css, _variables.css) for global styling.
    - Added component-specific styles for About Me, Profile, Resume, Badges, Badge, Contact Me, and Projects.
    - Introduced layout styles for Header and Footer.
    - Deleted old globals.css and organized styles into a more modular structure.
    - Updated _projects.css to enhance card layout and responsiveness.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,16 +2,7 @@ import { Inter } from 'next/font/google'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 
-import '../styles/globals.css'
-import '../styles/Header.css'
-import '../styles/Footer.css'
-import '../styles/Profile.css'
-import '../styles/AboutMe.css'
-import '../styles/Resume.css'
-import '../styles/Badges.css'
-import '../styles/Badge.css'
-import '../styles/ContactMe.css'
-import '../styles/Projects.css'
+import '../styles/main.css'
 import Script from 'next/script'
 
 config.autoAddCss = false
